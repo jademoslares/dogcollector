eac8cd5b846c82fa512676601bf52dc18cbed0ef
jademoslares <jade.moslares@gmail.com>
2024-03-11 14:15:40 -0400

    First part done

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+dogs = [
+  {'name': 'Itsuki', 'breed': 'Husky', 'description': 'Elegant and proud', 'age': 3},
+  {'name': 'Hisui', 'breed': 'Shiba Inu', 'description': 'Smart and Energetic', 'age': 2},
+]
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def dogs_index(request):
+  return render(request, 'dogs/index.html', {'dogs': dogs})
